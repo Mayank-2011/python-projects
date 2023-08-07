02831fd84c7deecd3589ceea2cf799a259fbf5c8
Mayank-2011 <131656202+Mayank-2011@users.noreply.github.com>
2023-08-07 12:20:51 +0530

    Create main.py

---
+++ ISS_overhead/main.py
@@ -0,0 +1,54 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_LAT = 25.437500  # Your longitude
+MY_LONG = 75.646301  # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+
+# Your position is within +5 or -5 degrees of the ISS position.
+def close_to_my_location():
+    if -5 <= MY_LAT - iss_latitude <= 5 and -5 <= MY_LONG - iss_longitude <= 5:
+        return True
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now().hour - 6   # -6 because subtracting timezone difference as sunrise and sunset are in UTC and this code is running on IST timezone machine
+
+# If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+while True:
+    time.sleep(60)
+    if close_to_my_location() and sunrise < time_now > sunset:
+        my_email = "<your email"
+        password = "<your app password"
+        smtp_server = 
+        with smtplib.SMTP(smtp_server, port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=my_email,
+                msg=f"Subject:ISS IS HERE.\n\nHey! Look up ISS is above you")
+
